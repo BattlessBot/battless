9ed59934fb5aade7def43d45d82e65ac27f1c35f
Battless <battless@example.com>
2026-02-02 02:27:16 +0000

    Fix API route to use public Edge Config token

--- api/thoughts.js
+++ api/thoughts.js
@@ -3,13 +3,9 @@ export const config = {
 };
 
 export default async function handler() {
+  // Use Edge Config's public read token
   const thoughts = await fetch(
-    'https://edge-config.vercel.com/ecfg_mleedjj4syhzruiqnpdqgfwxftqf/item/thoughts',
-    {
-      headers: {
-        'Authorization': 'Bearer ' + process.env.VERCEL_TOKEN
-      }
-    }
+    'https://edge-config.vercel.com/ecfg_mleedjj4syhzruiqnpdqgfwxftqf/item/thoughts?token=31cf2f41-1276-4170-886a-7eff94857cfd'
   ).then(r => r.json());
 
   return new Response(JSON.stringify(thoughts), {
